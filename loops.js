91b3763151b5b3db0998cf24df1b0ce10fe52734
Kyle Samuel <ksamuel92@gmail.com>
2021-02-06 19:28:37 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop (array) {
+  for (let i=0; i <= 25; i++) {
+    if (i === 1) {
+      array.push('I am 1 strange loop');
+    } else { 
+      array.push(`"I am ${i} strange loop${i === 0 ? '':'s'}`);
+    }
+  }
+  return array;
+}
+
+function whileLoop (n) {
+  while (n > 0) {
+    console.log (`Countdown: ${n}.`)
+    n--
+  }
+  return 'done';
+}
+
+function doWhileLoop (num) {
+   function incrementVariable() {
+     var i = 0;
+  i = i + 1;
+  return i;
+}
+  do {
+  incrementVariable();
+  console.log('I run once regardless.')
+} while (incrementVariable() < num );
+  
+}
